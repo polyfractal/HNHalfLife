3d47f4175d4d6aa823b1e94b70c6b6a02e90b925
Zachary Tong <zacharyjtong@gmail.com>
2011-11-30 22:53:14 -0500

    Formatting changes, commented out some unused variables which were collecting meta-data on the homepage

--- HNHalfLife.user.js
+++ HNHalfLife.user.js
@@ -44,184 +44,185 @@
 
 
 
+
 function timeAgoToDate(timeAgo) {
-    var matches = timeAgo.match(/([0-9]{0,3}) (minutes?|hours?|days?|years?) ago/);
-    var num = matches[1];
-    var type = matches[2];
+	var matches = timeAgo.match(/([0-9]{0,3}) (minutes?|hours?|days?|years?) ago/),
+		num = matches[1],
+		type = matches[2],
+		epochTime = new Date().getTime();
 
-    var epochTime = new Date().getTime();
+	if (type === "minutes" || type === "minute") {
+		epochTime -= num * 60000;
+	} else if (type === "hours" || type === "hour") {
+		epochTime -= num * 3600000;
+	} else if (type === "days" || type === "day") {
+		epochTime -= num * 86400000;
+	} else if (type === "years" || type === "year") {
+		epochTime -= num * 86400000 * 365;
+	}
 
-    if (type == "minutes" || type == "minute")
-        epochTime -= num * 60000;
-    else if (type == "hours" || type == "hour")
-        epochTime -= num * 3600000;
-    else if (type == "days" || type == "day")
-        epochTime -= num * 86400000;
-    else if (type == "years" || type == "year")
-        epochTime -= num * 86400000 * 365;
-
-    return epochTime;
-};
+	return epochTime;
+}
 
 
 var user = $("span.pagetop a[href^=user]").attr("href").match(/user\?id=([A-Za-z0-9_]+)/)[1];
 $("span.pagetop:first").append(" | <a href='http://news.ycombinator.com/saved?id=" + user + "'>saved</a>");
 
 
-if (window.location.pathname == "/item") {
+if (window.location.pathname === "/item") {
 
 
-    //find the first subtext table row and extract important information
-    var $subtext = $("td.subtext:first");
-    var id = $subtext.find("a[href^=item]").attr("href").match(/item\?id=([0-9]+)/)[1];
-    var numComments = $subtext.find("a[href^=item]").text().match(/([0-9]+) comments?/)[1];
-    var timeAgo = $subtext.text().match(/([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
-    var timeStamp = timeAgoToDate(timeAgo[1]);
+	//find the first subtext table row and extract important information
+	var $subtext = $("td.subtext:first");
+	var id = $subtext.find("a[href^=item]").attr("href").match(/item\?id=([0-9]+)/)[1];
+	var numComments = $subtext.find("a[href^=item]").text().match(/([0-9]+) comments?/)[1];
+	var timeAgo = $subtext.text().match(/([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
+	var timeStamp = timeAgoToDate(timeAgo[1]);
 
 
-    //check to see if this story is in our storage
-    //$.jStorage.deleteKey(id.toString());
-    var story = $.jStorage.get(id.toString());
+	//check to see if this story is in our storage
+	//$.jStorage.deleteKey(id.toString());
+	var story = $.jStorage.get(id.toString());
 
-    if(!story){
-        var lastVisited = new Date().getTime();
-        var dataToInsert =
-        {
-            "numComments" : numComments,
-            "timeStamp" : timeStamp,
-            "lastVisited" : lastVisited
-        };
+	if(!story){
+		var lastVisited = new Date().getTime();
+		var dataToInsert =
+		{
+			"numComments" : numComments,
+			"timeStamp" : timeStamp,
+			"lastVisited" : lastVisited
+		};
 
-        $.jStorage.set(id.toString(),dataToInsert);
-    }
-    else {
-        var commentDiff = numComments - story.numComments;
+		$.jStorage.set(id.toString(),dataToInsert);
+	}
+	else {
+		var commentDiff = numComments - story.numComments;
 
-        //update the subtext at top of story
-        if (commentDiff ==1 )
-            $subtext.append(" | " + commentDiff.toString() + " New Comment");
-        else if (commentDiff > 1 )
-            $subtext.append(" | " + commentDiff.toString() + " New Comments");
+		//update the subtext at top of story
+		if (commentDiff ===1 )
+			$subtext.append(" | " + commentDiff.toString() + " New Comment");
+		else if (commentDiff > 1 )
+			$subtext.append(" | " + commentDiff.toString() + " New Comments");
 
-        //find all new comments if they exist
-        if (commentDiff >= 1 ){
+		//find all new comments if they exist
+		if (commentDiff >= 1 ){
 
-            //really gross inline HTML here, needs a better solution
-            $('img[src$="s\.gif"][width="0"]:first')
-                .parents()
-                .eq(7)
-                .prepend('<table id="newComments" style="margin-left:7px"><thead><tr><td class="comhead" colspan="2"><a href="#" id="showComments">Show New Comments</a></td></tr></thead></table>');
+			//really gross inline HTML here, needs a better solution
+			$('img[src$="s\.gif"][width="0"]:first')
+				.parents()
+				.eq(7)
+				.prepend('<table id="newComments" style="margin-left:7px"><thead><tr><td class="comhead" colspan="2"><a href="#" id="showComments">Show New Comments</a></td></tr></thead></table>');
 
-            $('<tbody id="newCommentsBody"></tbody>').css("display","none").appendTo("#newComments");
-            $('<tfooter><tr><td><br/></td></tr></footer>').appendTo("#newComments");
-            
-
-            var potentialNewComments = new Array();
-
-            $("span.comhead:not(:first)").each(function() {
-                var commentDetails = $(this).text().match(/([A-Za-z0-9_]+) ([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
-                var commentAuthor = commentDetails[1];
-                var commentTimeStamp = timeAgoToDate(commentDetails[2]);
-                var commentText = $(this).parents().eq(1).find("span.comment > font").text().substring(0,80);
+			$('<tbody id="newCommentsBody"></tbody>').css("display","none").appendTo("#newComments");
+			$('<tfooter><tr><td><br/></td></tr></footer>').appendTo("#newComments");
 
 
-                if(commentTimeStamp > story.lastVisited){
+			var potentialNewComments = new Array();
 
-                    //because HN uses fuzzy timestamps, we have to create a list of "potential"
-                    //new comments.
-                    //
-                    //e.g. You read a post 1.2 hours ago.  A comment was made 1.3 hours ago.
-                    //     HN will round this to "1 hour ago", making it look like a new comment
-                    //
-                    //To account for this, we store comments now and check by ID later
-                    var tmp =
-                    {
-                        "commentDOMObject" : this,
-                        "commentID" : $(this).find("a[href^=item]").attr("href").match(/item\?id=([0-9]+)/)[1],
-                        "commentText" : commentText,
-                        "commentAuthor" : commentAuthor
-                    };
-                    potentialNewComments.push(tmp);
-
-                }
-            });
-
-            //sorts descending, so most recent comments (largest numbers) are the first keys
-            potentialNewComments.sort(function(a, b) {
-                return a.commentID < b.commentID;
-            });
-
-            //this should never happen, but just in case...
-            if (potentialNewComments.length < commentDiff)
-                commentDiff = potentialNewComments.length;
-
-            //finally, add the indicator for new comments.  Also adds to "comment list" at top of page
-            //really gross inline HTML here, needs a better solution
-            var tIndex;
-            for (tIndex=0;tIndex<commentDiff;++tIndex) {
-                var aNameID = '<a name="' + potentialNewComments[tIndex].commentID + '"></a>';
-                $(" <span>" + aNameID + " *New Comment*</span>").css("color","#FF6C0A").appendTo(potentialNewComments[tIndex].commentDOMObject);
-                $("<tr><td><img border='0' width='10' height='1' src='http://ycombinator.com/images/s.gif'></td><td class='default'><span class='comhead'>" + potentialNewComments[tIndex].commentAuthor + " says: <a style='text-decoration: underline' href='#" + potentialNewComments[tIndex].commentID + "'> " + potentialNewComments[tIndex].commentText + "[...]</a></span></td></tr>").appendTo("#newCommentsBody");
-            }
+			$("span.comhead:not(:first)").each(function() {
+				var commentDetails = $(this).text().match(/([A-Za-z0-9_]+) ([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
+				var commentAuthor = commentDetails[1];
+				var commentTimeStamp = timeAgoToDate(commentDetails[2]);
+				var commentText = $(this).parents().eq(1).find("span.comment > font").text().substring(0,80);
 
 
+				if(commentTimeStamp > story.lastVisited){
 
-        }
-        //update the datastore after 5s so "new" comments are removed after viewing
-        //pre-cache the time so we don't miss any comments during the 10s waiting period
-        var newLastVisited = new Date().getTime();
+					//because HN uses fuzzy timestamps, we have to create a list of "potential"
+					//new comments.
+					//
+					//e.g. You read a post 1.2 hours ago.  A comment was made 1.3 hours ago.
+					//     HN will round this to "1 hour ago", making it look like a new comment
+					//
+					//To account for this, we store comments now and check by ID later
+					var tmp =
+					{
+						"commentDOMObject" : this,
+						"commentID" : $(this).find("a[href^=item]").attr("href").match(/item\?id=([0-9]+)/)[1],
+						"commentText" : commentText,
+						"commentAuthor" : commentAuthor
+					};
+					potentialNewComments.push(tmp);
 
-        setTimeout(function(){
-            story.numComments = numComments;
-            story.lastVisited = newLastVisited;
-            $.jStorage.set(id.toString(),story);
-        },5000);
+				}
+			});
 
-    }
+			//sorts descending, so most recent comments (largest numbers) are the first keys
+			potentialNewComments.sort(function(a, b) {
+				return a.commentID < b.commentID;
+			});
+
+			//this should never happen, but just in case...
+			if (potentialNewComments.length < commentDiff)
+				commentDiff = potentialNewComments.length;
+
+			//finally, add the indicator for new comments.  Also adds to "comment list" at top of page
+			//really gross inline HTML here, needs a better solution
+			var tIndex;
+			for (tIndex=0;tIndex<commentDiff;++tIndex) {
+				var aNameID = '<a name="' + potentialNewComments[tIndex].commentID + '"></a>';
+				$(" <span>" + aNameID + " *New Comment*</span>").css("color","#FF6C0A").appendTo(potentialNewComments[tIndex].commentDOMObject);
+				$("<tr><td><img border='0' width='10' height='1' src='http://ycombinator.com/images/s.gif'></td><td class='default'><span class='comhead'>" + potentialNewComments[tIndex].commentAuthor + " says: <a style='text-decoration: underline' href='#" + potentialNewComments[tIndex].commentID + "'> " + potentialNewComments[tIndex].commentText + "[...]</a></span></td></tr>").appendTo("#newCommentsBody");
+			}
 
 
 
-    //event handler for "comment list"
-    $("#showComments").click(function(e){
-        if ($("#newCommentsBody").is(":visible"))
-            $(this).text("Show New Comments");
-        else
-            $(this).text("Hide New Comments");
+		}
+		//update the datastore after 5s so "new" comments are removed after viewing
+		//pre-cache the time so we don't miss any comments during the 10s waiting period
+		var newLastVisited = new Date().getTime();
 
-       $("#newCommentsBody").toggle();
-       e.preventDefault();
-    });
+		setTimeout(function(){
+			story.numComments = numComments;
+			story.lastVisited = newLastVisited;
+			$.jStorage.set(id.toString(),story);
+		},5000);
+
+	}
+
+
+
+	//event handler for "comment list"
+	$("#showComments").click(function(e){
+		if ($("#newCommentsBody").is(":visible"))
+			$(this).text("Show New Comments");
+		else
+			$(this).text("Hide New Comments");
+
+	   $("#newCommentsBody").toggle();
+	   e.preventDefault();
+	});
 
 }
 //update main index pages with "New Comments" text
 else if (window.location.pathname.match(/\/(?:news|newest|ask|best|active|noobstories|saved|x)/)) {
-    $("td.subtext").each(function() {
-        var id = $(this).find("a[href^=item]").attr("href").match(/item\?id=([0-9]+)/)[1];
+	$("td.subtext").each(function() {
+		var id = $(this).find("a[href^=item]").attr("href").match(/item\?id=([0-9]+)/)[1];
 
-        //check to see if this story is in our storage
-        var story = $.jStorage.get(id.toString());
+		//check to see if this story is in our storage
+		var story = $.jStorage.get(id.toString());
 
-        if(story){
-            var numComments = $(this).find("a[href^=item]").text().match(/([0-9]+) comments?/)[1];
-            var timeAgo = $(this).text().match(/([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
-            var timeStamp = timeAgoToDate(timeAgo[1]);
+		if(story){
+			var numComments = $(this).find("a[href^=item]").text().match(/([0-9]+) comments?/)[1];
+			//var timeAgo = $(this).text().match(/([0-9]{0,3} (?:minutes?|hours?|days?|years?) ago)/);
+			//var timeStamp = timeAgoToDate(timeAgo[1]);
 
-            var commentDiff = numComments - story.numComments;
+			var commentDiff = numComments - story.numComments;
 
-            //update the subtext at top of story
-            //Note: Is there a better way to add a vertical bar without styling, other than performing two appends?
-            if (commentDiff == 1 ){
-                //$(this).append(" | ");
-                $("<span> (" + commentDiff.toString() + " New Comment)</span>").css("color","#FF6C0A").appendTo(this);
-            }
-            else if (commentDiff > 1 ){
-                //$(this).append(" | ");
-                $("<span> (" + commentDiff.toString() + " New Comments)</span>").css("color","#FF6C0A").appendTo(this);
-            }
+			//update the subtext at top of story
+			//Note: Is there a better way to add a vertical bar without styling, other than performing two appends?
+			if (commentDiff === 1 ){
+				//$(this).append(" | ");
+				$("<span> (" + commentDiff.toString() + " New Comment)</span>").css("color","#FF6C0A").appendTo(this);
+			}
+			else if (commentDiff > 1 ){
+				//$(this).append(" | ");
+				$("<span> (" + commentDiff.toString() + " New Comments)</span>").css("color","#FF6C0A").appendTo(this);
+			}
 
 
-        }
-    });
+		}
+	});
 }
 
 
